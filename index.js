096663fa45040e7138901415e9e9f111f91e0702
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-19 13:25:19 +0600

    mongodb username and password hide

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get(
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://avenger-toys:U1QOOTc1Bv5iW9AR@cluster0.hltgyxi.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hltgyxi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
