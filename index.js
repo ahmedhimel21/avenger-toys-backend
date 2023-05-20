3b72b6f571dece5d1837c32a386c9e2544497880
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-20 15:33:34 +0600

    get operation by user email address

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get(
   '/',
-  (req,res) =>{
+  (req, res) => {
     res.send('running')
   }
 );
@@ -38,33 +38,54 @@ async function run() {
     const toysCollections = client.db('toysDB').collection('toysCollection');
 
     // specific data get by subCategory
-    app.get('/toys/:subCategory', async (req,res) =>{
-      const cursor = toysCollections.find({subCategory: req.params.subCategory})
+    app.get('/toys/:subCategory', async (req, res) => {
+      const cursor = toysCollections.find({ subCategory: req.params.subCategory })
       const result = await cursor.toArray();
       res.send(result)
     })
 
     // specific data get by _id
-    app.get('/viewDetails/:id', async(req,res) =>{
+    app.get('/viewDetails/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await toysCollections.findOne(query);
       res.send(result)
     })
 
     // get all data
-    app.get('/allToys', async(req,res) =>{
+    app.get('/allToys', async (req, res) => {
       const cursor = toysCollections.find().limit(20);
       const result = await cursor.toArray();
       res.send(result)
     })
 
     // post
-    app.post('/addAToy',async(req,res) => {
+    app.post('/addAToy', async (req, res) => {
       const addToy = req.body;
       const result = await toysCollections.insertOne(addToy);
       res.send(result)
       console.log(addToy);
+    })
+
+    // read some data
+    app.get('/addAToy', async (req, res) => {
+      // console.log(req.query);
+      // const result = await toysCollections.find().toArray()
+      // res.send(result)
+      try {
+        if (req.query.sellerEmail) {
+          const query = { sellerEmail: req.query.sellerEmail }
+          const sort = req?.query?.sort == 'true' ? 1 : -1;
+          const result = await toysCollections.find(query).sort({ price: sort }).toArray()
+          return res.send(result)
+        }
+        else{
+          res.status(404).send({error: true, message: 'User info not found'})
+        }
+      }
+      catch(error){
+        res.status(401).send({error: true, message: 'unauthorized'})
+      }
     })
 
     // Send a ping to confirm a successful connection
@@ -79,7 +100,7 @@ run().catch(console.dir);
 
 
 app.listen(
-  port, () =>{
+  port, () => {
     console.log(`Avenger toys API is running on port: ${port}`)
   }
 )
